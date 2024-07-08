7ab8920b3a92e56828a91568d5442f8fae149391
Yannoff <yannoff.it@gmail.com>
2024-07-08 08:35:14 +0200

    feat(update): allow full version choice

--- update.sh
+++ update.sh
@@ -5,35 +5,26 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( "$@" )
 if [ ${#versions[@]} -eq 0 ]; then
-	versions=( */ )
+    echo "Usage: $0 <full-version>"
+    exit 1
 fi
 versions=( "${versions[@]%/}" )
 
-read -r commit_hash fullVersion << EOF
-$(git ls-remote --tags https://github.com/adminerevo/adminerevo.git \
-	| awk '{gsub(/refs\/tags\/v/, "", $2); print}' \
-	| sort -rVk2 \
-	| head -1)
-EOF
-
-for version in "${versions[@]}"; do
-	if [[ "$fullVersion" != $version* ]]; then
-		echo >&2 "error: cannot determine full version for '$version'"
-	fi
-
-	echo "$version: $fullVersion"
-
+for fullVersion in "${versions[@]}"; do
+    version=$(echo $fullVersion | awk -F "." '{ printf "%s", $1 }')
+    commit=$(git ls-remote --tags https://github.com/adminerevo/adminerevo.git | awk -v "prefix=refs/tags/v" -v "tag=${fullVersion}" '($2 ~ prefix "" tag) { printf "%s", $1;  }') 
+    echo "commit: $commit"
 	downloadSha256="$(
 		curl -fsSL "https://github.com/adminerevo/adminerevo/releases/download/v${fullVersion}/adminer-${fullVersion}.php" \
 			| sha256sum \
 			| cut -d' ' -f1
 	)"
-	echo "  - adminer-${fullVersion}.php: $downloadSha256"
+	echo "Version: $version - adminer-${fullVersion}.php: $downloadSha256"
 
 	sed -ri \
 		-e 's/^(ENV\s+ADMINER_VERSION\s+).*/\1'"$fullVersion"'/' \
 		-e 's/^(ENV\s+ADMINER_DOWNLOAD_SHA256\s+).*/\1'"$downloadSha256"'/' \
-		-e 's/^(ENV\s+ADMINER_COMMIT\s+).*/\1'"$commit_hash"'/' \
+		-e 's/^(ENV\s+ADMINER_COMMIT\s+).*/\1'"$commit"'/' \
 		"$version/fastcgi/Dockerfile" \
 		"$version/Dockerfile"
 done
